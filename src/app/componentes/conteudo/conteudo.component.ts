de1667f0920436089ae665f637ab998b86bb0c74
joao antonio <joao.esganzella@gmail.com>
2025-06-09 20:19:31 -0300

    bel x mia

--- src/app/componentes/conteudo/conteudo.component.ts
+++ src/app/componentes/conteudo/conteudo.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conteudo',
   templateUrl: './conteudo.component.html',
   styleUrls: ['./conteudo.component.css']
 })
-export class ConteudoComponent {
+export class ConteudoComponent implements OnInit {
+
+  public mensagem: string;
+  public valor: number;
+  public link_foto: string;
+
+  public constructor(){
+    this.mensagem = "Ola mundo";
+    this.valor = 0;
+    this.link_foto = "/assets/bellindao.png";
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  public mudarValor(){
+    this.valor++;
+    if(this.valor % 2 === 0){
+      this.link_foto = "/assets/miazinha.png"
+    } else {
+      this.link_foto = "/assets/bellindao.png";
+    }
+  }
 
 }
